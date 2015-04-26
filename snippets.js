c0b2549b375407dbd6756179f5f1de8c71d7fe02
Michael Bernstein <Michael.Bernstein@Contractor.cengage.com>
2015-04-26 08:29:31 -0400

    Begin implementation of mongoose and mongo

--- snippets.js
+++ snippets.js
@@ -1,4 +1,10 @@
 var request = require('request');
+var mongoose = require('mongoose');
+
+var databaseUri =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
 
 var getSnippets = function(req, res, callback) {
   var geoMapper = {
@@ -16,10 +22,36 @@ var getSnippets = function(req, res, callback) {
     style: 'full',
     maxRows: '25'
   };
+
   var options = {
     url: 'http://api.geonames.org/findNearbyWikipediaJSON',
     qs: queryParams
   };
+
+  mongoose.connect(databaseUri, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + databaseUri + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + databaseUri);
+    }
+  });
+
+  var snippetSchema = new mongoose.Schema({
+    guid: String,
+    url: String
+  });
+
+  var SnippetRecord = mongoose.model('SnippetRecords', snippetSchema);
+
+  // Creating one user.
+  var test123 = new SnippetRecord ({
+    guid: 'Hello',
+    url: 'World'
+  });
+
+  // Saving it to the database.  
+  test123.save(function (err) {if (err) console.log ('Error on save!')});
+
   request(options, function(err, response, body) {
     if (err) {
       callback({});
