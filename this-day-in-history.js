d6c147437fc73b208906023dddfc372dd79af3d1
Teddy Wing <git@teddywing.com>
2015-04-26 10:09:52 -0400

    this-day-in-history: Request param should just be day & month
    Since I don't feel like changing the backend API right now, just
    hard-code a year. Yee-haw!

--- this-day-in-history.js
+++ this-day-in-history.js
@@ -2,9 +2,9 @@ var request = require('request');
 
 module.exports = {
   get: function(date_string, callback) {
-    if (/\d{4}\/\d?\d\/\d?\d/.test(date_string)) {
+    if (/\d?\d\/\d?\d/.test(date_string)) {
       request({
-        url: 'http://hidden-sands-6666.herokuapp.com/api/'+ date_string +'/'
+        url: 'http://hidden-sands-6666.herokuapp.com/api/2015/'+ date_string +'/'
       }, function(err, response, body) {
         if (err) {
           callback({});
