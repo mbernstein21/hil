38c345f6f6f25b84b5b95f7224e84522678a2a99
Teddy Wing <git@teddywing.com>
2015-04-26 04:41:21 -0400

    index.js: Add images into `snippets`
    Grab the rest/tail of our images from 500px and stick them in
    `snippets`. Shuffle the snippets array and ensure it always starts with
    a text (instead of an image) snippet.

--- index.js
+++ index.js
@@ -13,12 +13,29 @@ app.get('/', function (req, res) {
     return;
   }
   
+  // From http://stackoverflow.com/a/6274381
+  function shuffle(arr) {
+    for (
+      var j, x, i = arr.length;
+      i;
+      j = Math.floor(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x
+    );
+    return arr;
+  }
+  
   photos.get(req.query.latitude, req.query.longitude, 1, function(images) {
     if (images.length) {
-      data.image = images[0];
+      data.image = images.shift();
     }
 
     snippets(req, res, function(snippets) {
+      // Shuffle images into the snippets array and make sure the first 
+      // snippet is always a text snippet
+      var first = snippets.shift();
+      snippets = snippets.concat(images);
+      snippets = shuffle(snippets);
+      snippets.unshift(first)
+      
       data.snippets = snippets;
       res.send(data);
     });
